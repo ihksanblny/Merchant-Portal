d414ccb7022fee3db30146a31a2ab88c36169718
ihksanblny <isanomuzumi878@gmail.com>
2026-04-11 00:49:01 +0700

    init folder

---
+++ database/migrations/2026_04_10_173913_create_batches_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('batches', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->string('batch_code')->nullable();
+            $table->date('expiry_date')->nullable();
+            $table->integer('initial_qty');
+            $table->integer('current_qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('batches');
+    }
+};
